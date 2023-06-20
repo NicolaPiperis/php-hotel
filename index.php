28a0f7868f089ad9b9cf4405d79022360da53293
NicolaPiperis <nicolapiperis9999@gmail.com>
2023-06-20 15:29:22 +0200

    base structure

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>php-hotel</title>
+    </head>
+    <body>
+
+        <?php
+            $hotels = [
+
+                [
+                    'name' => 'Hotel Belvedere',
+                    'description' => 'Hotel Belvedere Descrizione',
+                    'parking' => true,
+                    'vote' => 4,
+                    'distance_to_center' => 10.4
+                ],
+                [
+                    'name' => 'Hotel Futuro',
+                    'description' => 'Hotel Futuro Descrizione',
+                    'parking' => true,
+                    'vote' => 2,
+                    'distance_to_center' => 2
+                ],
+                [
+                    'name' => 'Hotel Rivamare',
+                    'description' => 'Hotel Rivamare Descrizione',
+                    'parking' => false,
+                    'vote' => 1,
+                    'distance_to_center' => 1
+                ],
+                [
+                    'name' => 'Hotel Bellavista',
+                    'description' => 'Hotel Bellavista Descrizione',
+                    'parking' => false,
+                    'vote' => 5,
+                    'distance_to_center' => 5.5
+                ],
+                [
+                    'name' => 'Hotel Milano',
+                    'description' => 'Hotel Milano Descrizione',
+                    'parking' => true,
+                    'vote' => 2,
+                    'distance_to_center' => 50
+                ],
+
+            ];
+
+            // echo "<ul>";
+
+            foreach ($hotels as $hotel) {
+                // echo "<li>" . $hotels . "</li>";
+                var_dump($hotel)
+            }
+
+            // echo "</ul>";
+        ?>
+        
+    </body>
+</html>
+
+
+<!-- 
+Stampare tutti i nostri hotel con tutti i dati disponibili.
+
+Iniziare in modo graduale:
+Prima stampate in pagina i dati, senza preoccuparvi dello stile (vedi var_dump). Dopo aggiungete Bootstrap e mostrate le informazioni con una tabella.
+
+Bonus
+Aggiungere un form ad inizio pagina che tramite una richiesta GET permetta di filtrare gli hotel che hanno un parcheggio
+Aggiungere un secondo campo al form che permetta di filtrare gli hotel per voto (es. inserisco 3 ed ottengo tutti gli hotel che hanno un voto di tre stelle o superiore)
+
+Nota Bene
+Deve essere possibile utilizzare entrambi i filtri contemporaneamente (es. ottenere una lista con hotel che dispongono di parcheggio e che hanno un voto di tre stelle o superiore)
+Se non viene specificato nessun filtro, visualizzare come in precedenza tutti gli hotel 
+-->
